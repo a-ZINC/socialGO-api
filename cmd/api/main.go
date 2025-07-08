f6d845aea303cd9c6f7de5417858d8c051901a26
a-ZINC <azinc28@gmail.com>
2025-07-09 01:34:22 +0530

    env setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"social-api/internal/env"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
 	app := &Application{
 		Config: Config{
-			Addr: ":4000",
+			Addr: ":" + env.GetString("ADDR", "3000"),
 		},
 	}
 	mux := app.mount()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module social-api
 
 go 1.24.3
 
-require github.com/go-chi/chi/v5 v5.2.2
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/joho/godotenv v1.5.1
+)
